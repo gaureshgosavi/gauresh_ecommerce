60e3bd1bb035d30a699b6dcc51b33df36661226a
Gauresh Gosavi <gaureshgosavi10@gmail.com>
2016-11-09 23:42:19 +0530

    fourth commit

--- gauresh_BackEnd/src/main/java/com/niit/gauresh_backend/config/ApplicationConfig.java
+++ gauresh_BackEnd/src/main/java/com/niit/gauresh_backend/config/ApplicationConfig.java
@@ -1,60 +1,61 @@
 package com.niit.gauresh_backend.config;
 
-
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScan
+@ComponentScan("com.niit")
 public class ApplicationConfig {
-	   @Autowired
-	    private Environment environment;
-	
-	  @Bean
-	    public DataSource dataSource() {
-	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
-	        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
-	        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
-	        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
-	        return dataSource;
-	    }
-	  
-	  private Properties hibernateProperties() {
-	        Properties properties = new Properties();
-	        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-	        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-	        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-	        return properties;        
-	    }
-	  
-	  @Bean
-	    public LocalSessionFactoryBean sessionFactory() {
-	        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-	        sessionFactory.setDataSource(dataSource());
-	        sessionFactory.setPackagesToScan(new String[] { "com.websystique.springmvc.model" });
-	        sessionFactory.setHibernateProperties(hibernateProperties());
-	        return sessionFactory;
-	     }
-	  
-	    @Bean
-	    @Autowired
-	    public HibernateTransactionManager transactionManager(SessionFactory s) {
-	       HibernateTransactionManager txManager = new HibernateTransactionManager();
-	       txManager.setSessionFactory(s);
-	       return txManager;
-	    }
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+		return dataSource;
+	}
+
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", "hibernate.dialect");
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+		return properties;
+	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan(new String[] { "com.niit.gauresh_backend.model" });
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		return sessionFactory;
+	}
+
+	@Autowired
+	@Bean
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory);
+		return transactionManager;
+	}
 
 }
